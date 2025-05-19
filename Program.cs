f6a78b126640bbdb5db9398b065c02422c25f4ab
Aiden Turner <yosefaiden@gmail.com>
2025-05-19 14:33:32 -0400

    finished.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CommandLine;
+
+class ListFilesApp {
+    public class Options {
+        [Option("filename", Required = false, HelpText = "Input filename.")]
+        public string? Filename { get; set; }
+    }
+
+    string[] GetAllFiles(string filename) {
+        string[] files = Directory.GetFiles(filename, "*", SearchOption.TopDirectoryOnly);
+        string[] directories = Directory.GetDirectories(filename, "*", SearchOption.TopDirectoryOnly);
+        return files.Concat(directories).ToArray();
+    }
+
+    void WriteFilesToDateNamedFile(string[] files) {
+        string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+        string dateFileName = $"file_list_{timestamp}.txt";
+        File.WriteAllLines(dateFileName, files);
+        Console.WriteLine($"File list written to: {dateFileName}");
+    }
+
+    static int Main(string[] args) {
+        var app = new ListFilesApp();
+        Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+            if (o.Filename != null) {
+                if(!Directory.Exists(o.Filename)) {
+                    Console.WriteLine("Please input a valid, existing directory.");
+                    throw new FileNotFoundException();
+                }
+                string[] allFiles = app.GetAllFiles(o.Filename);
+                foreach(string files in allFiles) {
+                    Console.WriteLine(files);
+                }
+                app.WriteFilesToDateNamedFile(allFiles);
+            }
+            else {
+                Console.WriteLine("Input a valid, existing directory: ");
+                string? filename = Console.ReadLine();
+
+                if(!Directory.Exists(filename)) {
+                    Console.WriteLine("Please input a valid, existing directory.");
+                    throw new FileNotFoundException();
+                }
+
+                string[] allFiles = app.GetAllFiles(filename);
+                foreach(string files in allFiles) {
+                    Console.WriteLine(files);
+                }
+                app.WriteFilesToDateNamedFile(allFiles);
+            }
+        });
+        return 0;
+    }
+}
